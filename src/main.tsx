5f3ae89a8002cb877e16d949c48de8d1b3921fcf
Marnitz Malan <marnitz.malan@payfast.co.za>
2025-01-14 23:48:30 +0200

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { AuthProvider } from "@/context/AuthProvider";
 import router from "./Router"
-import "@/assets/styles/main.scss"
+import "@/assets/styles/main.css"
 
 const queryClient = new QueryClient({
     defaultOptions: {
